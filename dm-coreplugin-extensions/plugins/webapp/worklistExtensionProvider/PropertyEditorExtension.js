81e812093ca942c7449f0351e43237e60f2bf742
Vivek <vivekananda.panigrahy@sap.com>
2023-05-11 00:43:07 -0400

    Core Plugin Extension for OrderSelectList Plugin and other fixes

---
+++ dm-coreplugin-extensions/plugins/webapp/worklistExtensionProvider/PropertyEditorExtension.js
@@ -0,0 +1,76 @@
+sap.ui.define([
+    "sap/dm/dme/podfoundation/extension/PluginPropertyEditorExtension"
+], function (PluginPropertyEditorExtension) {
+    "use strict";
+
+    return PluginPropertyEditorExtension.extend("sap.example.plugins.worklistExtensionProvider.PropertyEditorExtension", {
+        constructor: function (oExtensionUtilities) {
+            this._oExtensionUtilities = oExtensionUtilities;
+        },
+
+        /*
+         * Function to override to add content before core properties
+         *
+         * @param {sap.ui.layout.form.SimpleForm} oPropertyFormContainer Form to add controls to
+         * @param {object} oPropertyData Defined Property Data
+         * @override
+         */
+        addPropertyEditorContentBefore: function (oPropertyFormContainer, oPropertyData) {
+            // to be implemented by sub-classes
+            this._oExtensionUtilities.logMessage("PropertyEditorExtension.addPropertyEditorContentBefore: hi");
+            return;
+        },
+
+        /*
+         * Function to override to add content after core properties
+         *
+         * @param {sap.ui.layout.form.SimpleForm} oPropertyFormContainer Form to add controls to
+         * @param {object} oPropertyData Defined Property Data
+         * @override
+         */
+        addPropertyEditorContentAfter: function (oPropertyFormContainer, oPropertyData) {
+            // to be implemented by sub-classes
+            this._oExtensionUtilities.logMessage("PropertyEditorExtension.addPropertyEditorContentAfter: hi");
+            return;
+        },
+
+        /*
+         * Function to override to add custom default property values to core property data
+         *
+         * @param {object} oPropertyData Defined Property Data
+         * @returns {object} Updated Property Data
+         * @override
+         */
+        getPropertyData: function (oPropertyData) {
+            // to be implemented by sub-classes
+            this._oExtensionUtilities.logMessage("PropertyEditorExtension.getPropertyData: hi");
+            return oPropertyData;
+        },
+
+        /*
+         * Function to override to add custom default property values to core property data
+         *
+         * @param {object} oPropertyData Defined Property Data
+         * @returns {object} Updated Property Data
+         * @override
+         */
+        setPropertyData: function (oPropertyData) {
+            // to be implemented by sub-classes
+            this._oExtensionUtilities.logMessage("PropertyEditorExtension.setPropertyData: hi");
+            return oPropertyData;
+        },
+
+        /*
+         * Function to override to add custom default property values
+         *
+         * @param {object} oPropertyData Defined Property Data
+         * @returns {object} Updated Property Data
+         * @override
+         */
+        getDefaultPropertyData: function (oPropertyData) {
+            // to be implemented by sub-classes
+            this._oExtensionUtilities.logMessage("PropertyEditorExtension.getDefaultPropertyData: hi");
+            return oPropertyData;
+        }
+    })
+});
