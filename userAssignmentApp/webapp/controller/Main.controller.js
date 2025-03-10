16426b7361a1c0d142f8c115378979fdd123b783
Katja Huschle <katja.huschle@sap.com>
2025-03-10 14:55:59 +0000

    userAssignmentApp first successful deployment

---
+++ userAssignmentApp/webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("userAssignmentApp.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ userAssignmentApp/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"userAssignmentApp/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
